53a5565ffce675b68d7a2162b41f5fabc8c67a32
FankSid <fanksid@gmail.com>
2016-04-15 21:28:00 +0800

    codes

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,45 @@
 package com.twu.biblioteca;
 
+import java.util.Scanner;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    	// login
+    	int usrcode = 0;
+        System.out.println("Hello there, welcome to the library.\n"
+        		+ "Press 1 to login as customer and press other characters to login as librarian.");
+        Scanner inp =new Scanner(System.in);
+        String str = inp.nextLine();
+        usrcode = Integer.parseInt(str);
+        if(usrcode == 1)
+        	System.out.println("Welcome, customer.\n"
+        			+ "Press 1 to enter the Menu opinon: \'List Books\'.");
+        else
+        	System.out.println("Welcome, librarian.\n"
+        			+ "Press 1 to enter the Menu opinon: \'List Books\'.");
+        // to see the main menu
+        int menucode = 0;
+        while(menucode != 1){
+	        inp =new Scanner(System.in);
+	        str = inp.nextLine();
+	        menucode =  Integer.parseInt(str);
+	        if(menucode != 1)
+	        	System.out.println("Please select a valid option!.");
+	        };
+        // to see the book list
+	    int[] books = new int[10];
+	    int[] bookstatus = new int[10];
+	    for(int i = 1;i<=10;i++){
+	    	books[i-1] = i;
+	    	bookstatus[i-1] = 0; // 0 means not checkout
+	    }
+	    //show the books
+	    if(menucode == 1){
+	    	for(int i = 0;i < 10; i++){
+	    		System.out.println("Book name: Book"+books[i]+". Status: "+(bookstatus[i]==0?"In Library":"Not in")+".");
+	    		}
+	    	menucode = 11;
+	    	}
     }
 }
